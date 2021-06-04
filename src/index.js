ffa66223c910689d0e5f8c421a6b6f7d377567f5
minh duc <hunter170701@gmail.com>
2021-06-05 02:44:57 +0700

    upload

--- src/index.js
+++ src/index.js
@@ -31,5 +31,5 @@ router.route(app);
 db.connect();
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
